aac8ebebb458cf2bc3ac16d8b1eadb781f6d00a0
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-10-26 18:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  const num=7;
+	const binaryEquivalent= num.toString(2);
+	console.log(binaryEquivalent);
 }
 
 window.decimalToBinary = decimalToBinary;
